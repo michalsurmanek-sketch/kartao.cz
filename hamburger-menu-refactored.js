f1b74880af64989d450cde55c3e9972a7e581366
michalsurmanek-sketch <michalsurmanek@gmail.com>
2025-12-10 17:48:12 +0100

    Change action to href for login menu item

--- hamburger-menu-refactored.js
+++ hamburger-menu-refactored.js
@@ -129,7 +129,7 @@ class HamburgerMenu {
         { href: 'kartao-pro-tvurce.html', icon: 'user-plus', text: 'Pro tvůrce' },
         { href: 'kartao-pro-firmy.html', icon: 'building', text: 'Pro firmy' },  
         { href: 'kontakt.html', icon: 'mail', text: 'Podpora' },
-        { action: 'login.html', icon: 'log-in', text: 'Přihlásit' }
+        { href: 'login.html', icon: 'log-in', text: 'Přihlásit' }
       ],
       creator: [
         { href: 'moje-karta.html', icon: 'sparkles', text: 'Moje Kartao' },
